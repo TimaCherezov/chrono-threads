a326964d3cb28ba9936bda78f7ea08d04042dd11
Vladislav Ershov <vlad0303@outlook.com>
2025-04-14 22:34:07 +0500

    door

---
+++ Assets/DoorActivity.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class DoorActivity : MonoBehaviour
+{
+    [SerializeField] private GameObject messageBox;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+            return;
+
+        messageBox.SetActive(true);
+        other.gameObject.GetComponent<PastHero>().IsAllowedMove = false;
+    }
+
+    // private void OnCollisionExit2D(Collision2D other)
+    // {
+    //     if (!other.gameObject.CompareTag("Player"))
+    //         return;
+    //
+    //     messageBox.SetActive(false);
+    // }
+}
