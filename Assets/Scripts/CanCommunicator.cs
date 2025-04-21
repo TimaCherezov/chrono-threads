5bdd8c515eddf6bebc77ba15467acb7880f00725
Vladislav Ershov <vlad0303@outlook.com>
2025-04-21 22:59:08 +0500

    health and cans

---
+++ Assets/Scripts/CanCommunicator.cs
@@ -0,0 +1,34 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CanCommunicator : MonoBehaviour
+{
+    [CanBeNull] private CanStateContainer target;
+
+    private void Update()
+    {
+        if (target is not null &&
+            Input.GetKeyDown(KeyCode.E))
+        {
+            target.SetState(!target.IsActive);
+            Debug.Log("CHAGED");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.TryGetComponent(typeof(CanStateContainer), out var can))
+            return;
+        Debug.Log("TURNED ON");
+        target = can as CanStateContainer;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.TryGetComponent(typeof(CanStateContainer), out _))
+            return;
+        Debug.Log("TURNED OFF");
+        target = null;
+    }
+}
---
+++ Assets/Scripts/HeroHealth.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] private int currentHealth = 10;
+    [SerializeField] private GameObject healthBar;
+    private Scrollbar _scrollbar;
+
+    private void Awake()
+    {
+        _scrollbar = healthBar.GetComponent<Scrollbar>();
+        _scrollbar.size = CalculateScrollbarSize();
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        currentHealth += Math.Clamp(damage,
+            -currentHealth,
+            maxHealth - currentHealth
+        );
+        _scrollbar.size = CalculateScrollbarSize();
+        Debug.Log($"Future hero has damage applied {damage}");
+    }
+
+    private float CalculateScrollbarSize() =>
+        Convert.ToSingle(currentHealth) / Convert.ToSingle(maxHealth);
+}
