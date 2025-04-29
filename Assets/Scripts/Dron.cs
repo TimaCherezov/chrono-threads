710b4cecb1118a559df9370da2fdf8ca03a52e43
TimaCherezov <timacherezov06@gmail.com>
2025-04-29 10:37:06 +0500

    fixed dron

--- Assets/Scripts/Dron.cs
+++ Assets/Scripts/Dron.cs
@@ -9,23 +9,36 @@ public class Dron : MonoBehaviour
     [SerializeField] private GameObject[] waypoints;
     private int currentWaypoint;
     [SerializeField] private GameObject bulletPrefab;
-    private bool isRotating = false;
-    private int rotatingTimes = 0;
+    private bool isRotating;
+    private int rotatingTimes;
 
     void FixedUpdate()
     {
         if (isRotating)
         {
-            Vector2 direction = waypoints[currentWaypoint].transform.position - transform.position;
-            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            var targetRotation = Quaternion.Euler(0, 0, angle);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1 * Time.deltaTime);
-            rotatingTimes++;
-            if (rotatingTimes >= 100)
-                isRotating = false;
-            return;
+            RotateTowardsCurrentWaypoint();
+            return; 
         }
 
+        MoveTowardsCurrentWaypoint();
+    }
+
+    private void RotateTowardsCurrentWaypoint()
+    {
+        var direction = waypoints[currentWaypoint].transform.position - transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var targetRotation = Quaternion.Euler(0, 0, angle);
+        var rotationSpeed = 90f;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if (Quaternion.Angle(transform.rotation, targetRotation) < 0.01f)
+        {
+            isRotating = false; 
+        }
+    }
+
+    private void MoveTowardsCurrentWaypoint()
+    {
         transform.position = Vector2.MoveTowards(
             transform.position,
             waypoints[currentWaypoint].transform.position,
@@ -35,10 +48,10 @@ public class Dron : MonoBehaviour
         if (Vector2.Distance(transform.position, waypoints[currentWaypoint].transform.position) < 0.1f)
         {
             currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
-            rotatingTimes = 0;
-            isRotating = true;
+            isRotating = true; 
         }
     }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
