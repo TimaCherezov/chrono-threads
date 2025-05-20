5cd6963f70e1ad055653d4d2284a73c89b0269b5
SsnyX <fan4cz@gmail.com>
2025-05-20 21:10:28 +0500

    refactor

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -16,7 +16,7 @@ public class Bullet : MonoBehaviour
         transform.position = Vector2.MoveTowards(
             transform.position, target.transform.position,
             1 * Time.deltaTime);
-        Vector2 direction = target.transform.position - transform.position;
+        var direction = target.transform.position - transform.position;
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         var targetRotation = Quaternion.Euler(0, 0, angle);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
@@ -25,7 +25,7 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name != "HeroTarget") return;
-        other.GetComponentInParent<HeroHealth>().ApplyDamage(-2);
+        other.GetComponentInParent<HeroHealth>().ApplyDamage(2);
         Destroy(gameObject);
     }
 }
